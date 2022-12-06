fafefade48090d2355f262115d21c17eb551bf10
Your Name <sarabian17@outook.com>
2022-12-06 19:34:53 -0300

    input perguntas data

--- ativ1.py
+++ ativ1.py
@@ -1,7 +1,7 @@
-from time import sleep
+ano = int(input("Ano de nascimento: \n"))
+mes = int(input("Mês de nascimento: \n"))
+dia = int(input("Dia de nascimento: \n"))
 
-
-for i in range(10, -1, -1):
-    print(i)
-    sleep(1)
-print("FELIZ ANO NOVO!")
+print("A pessoa nasceu em: {}/{}/{}".format(dia, mes, ano))
+print("A pessoa nasceu no dia {} do mês {} do ano de {}.".format(dia, mes, ano))
+     
